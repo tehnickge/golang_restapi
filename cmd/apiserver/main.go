7fd837f660ae923b84f5fd1c6ea2242d960a6ade
Nikita "Tehnick <71207932+tehnickge@users.noreply.github.com>
2024-09-28 18:04:26 +0300

    add router

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,12 +1,29 @@
-package main 
+package main
 
 import (
+	"flag"
 	"log"
+
+	"github.com/BurntSushi/toml"
 	"github.com/tehnickge/golang_restapi/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml","path to config file")
+}
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {log.Fatal(err)}
+
+	s := apiserver.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -3,10 +3,12 @@ package apiserver
 //config
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
+	logLevel string `toml:"log_level"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		logLevel: "debug",
 	}
 }
